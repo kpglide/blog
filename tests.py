e789d08908ba92232f066611a5aa1c366c5dcede
unknown <kglidewell@TBS-V9JL1SWKQXH.turner.com>
2014-05-27 16:37:44 -0400

    'updated'

--- tests.py
+++ tests.py
@@ -3,7 +3,8 @@ import unittest
 import config
 from config import basedir
 from app import app, db
-from app.models import User
+from app.models import User, Post
+from flask import url_for
 
 class TestCase(unittest.TestCase):
 
@@ -11,6 +12,7 @@ class TestCase(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
         app.config['TESTING'] = True
         app.config['CSRF_ENABLED'] = False
+        app.config['WTF_CSRF_ENABLED'] = False
         self.app = app.test_client()
         db.create_all()
 
@@ -19,10 +21,10 @@ class TestCase(unittest.TestCase):
         db.drop_all()
 
     def login(self, username, password):
-        return self.app.post('/admin', data=dict(
-        username=username,
-        password=password
-        ), follow_redirects=True)
+        return self.app.post('/admin', data={
+			'username':'testtest',
+			'password':'password'
+        }, follow_redirects=True)
 
     def logout(self):
         return self.app.get('/logout', follow_redirects=True)
@@ -31,13 +33,24 @@ class TestCase(unittest.TestCase):
         val = self.app.get('/')
         assert 'About' in val.data
 
-    def test_login_logout(self):
+    def test_create_user(self):
         u = User(username='testtest', password='password', role=1)
         db.session.add(u)
         db.session.commit()
-        """Make sure login and logout works"""
         u2 = User.query.first()
         assert u2.username == 'testtest' and u2.password == 'password'
+		
+    def test_home_page(self):
+        response = self.app.get('/index')
+        assert 'describing' in response.data
+		
+    def test_login(self):
+        u = User(username='testtest', password='password', role=1)
+        db.session.add(u)
+        db.session.commit()
+        response = self.login(username='testtest', password='password')
+        assert 'Post' in response.data
+	
 
 if __name__ == '__main__':
     unittest.main()
