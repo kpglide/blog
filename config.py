a66011174290a3b5a31ae134a5f0aa67c98f2212
Kevin Glidewell <kpglide@gmail.com>
2014-05-30 21:58:31 -0400

    updated

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'blog.db')
+if os.environ.get('DATABASE_URL') is None:
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'blog.db')
+else:
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 CSRF_ENABLED = True
-
-"""In production, change secret key so that it originates from
-an environment variable"""
-SECRET_KEY = 'test_secret_key'
+SECRET_KEY = ENV['SECRETKEY']
 
 POSTS_PER_PAGE = 5
